aa7d5baec65cfaace5b448b18132241f77b7674e
OduduU <49434847+OduduU@users.noreply.github.com>
2020-03-19 22:55:36 +0100

    implemet routing with react router and HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import MenuItem from '../menu-item/menu-item.component'
+import MenuItem from "../menu-item/menu-item.component";
 
-import './directory.styles.scss'
+import "./directory.styles.scss";
 
 class Directory extends React.Component {
 	constructor() {
@@ -13,7 +13,7 @@ class Directory extends React.Component {
 					title: "hats",
 					imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
 					id: 1,
-					linkUrl: "shop/hats"
+					linkUrl: "hats"
 				},
 				{
 					title: "jackets",
@@ -43,19 +43,17 @@ class Directory extends React.Component {
 				}
 			]
 		};
-    }
-    
-    render() {
-        return (
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(({ id, title, imageUrl, size }) => (
-						<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-                    ))
-                }
-            </div>
-        )
-    }
+	}
+
+	render() {
+		return (
+			<div className="directory-menu">
+				{this.state.sections.map(({ id, ...otherSectionProps }) => (
+					<MenuItem key={id} {...otherSectionProps} />
+				))}
+			</div>
+		);
+	}
 }
 
 export default Directory;
