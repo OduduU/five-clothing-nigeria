cf8ffca03125e58948ba63a9dbe3c289e8409b52
OduduU <49434847+OduduU@users.noreply.github.com>
2020-06-21 08:40:37 +0100

    separating our cra app into client folder, added express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button-component.jsx
+++ client/src/components/stripe-button/stripe-button-component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 import './stripe-button.stles.scss'
 
@@ -8,8 +9,19 @@ const StripeCheckoutButton = ({price}) => {
     const publishableKey = "pk_test_PmN7dN9HwijMPrUVsgRnVE9u006ShS1dUp";
 
     const onToken = token => {
-        console.log('token :', token);
-        alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(res => {
+            alert('Payment Successful')
+        }).catch(err => {
+            console.log('Payment error: ', JSON.parse(err));
+            alert('There was an issue with your payment, kindly confirm your test credit card.')
+        })
     }
 
     return (
--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -46,7 +46,7 @@ const Checkout = ({ cartItems, total }) => {
 			<div className="test-warning">
 				*Please use the following test credit card for payments*
 				<br />
-				4242 4242 4242 4242 - Exp: 05/20 - cvv: 123
+				4242 4242 4242 4242 - Exp: 08/20 - cvv: 123
 			</div>
 
 			<div className='pay-now'>
